018beaf2fae26fd6419618540f9d890fa0acb523
Sirlol555 <159081934+Sirlol555@users.noreply.github.com>
2024-09-10 20:18:51 -0500

    Shop Outline Created
    Shop panel was created, and the trigger to open and close the shop was created when the player moves close to the shop object

---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+    public GameObject shop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shop.SetActive(false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            shop.SetActive(true);
+        }
+        
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        shop.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

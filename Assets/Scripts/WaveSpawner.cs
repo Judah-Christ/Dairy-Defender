6adb597db8d756a199a10eddca2887d88b4dca5e
Sirlol555 <jppritchett@mail.bradley.edu>
2024-11-12 23:44:44 -0600

    update

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using Cinemachine;
+using TMPro;
 
 public class WaveSpawner : MonoBehaviour
 {
     public enum SpawnState { spawning, waiting, counting };
+    [SerializeField] CinemachineVirtualCamera Mapcam;
+    [SerializeField] private TextMeshProUGUI waveInfo;
+    private int countdownTime = 30;
 
     [System.Serializable]
     public class Wave
@@ -16,7 +21,7 @@ public class WaveSpawner : MonoBehaviour
         public Transform enemy2;
         public int count;
         public float rate;
-        
+
     }
 
     public Wave[] waves;
@@ -35,20 +40,24 @@ public class WaveSpawner : MonoBehaviour
 
     void Start()
     {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("no spawn points");
+        }
 
         waveCountdown = timeBetweenWaves;
 
-        
+
     }
 
     void Update()
     {
         if (state == SpawnState.waiting)
         {
-            if(!enemyisAlive()) 
+            if (!enemyisAlive())
             {
-               WaveCompleted();
-                
+                WaveCompleted();
+
             }
             else
             {
@@ -62,6 +71,7 @@ public class WaveSpawner : MonoBehaviour
             if (state != SpawnState.spawning)
             {
                 StartCoroutine(SpawnWave(waves[nextWave]));
+                StartCoroutine(MapcamCall());
             }
         }
         else
@@ -72,18 +82,21 @@ public class WaveSpawner : MonoBehaviour
 
     void WaveCompleted()
     {
+        Debug.Log("waveCompleted");
         state = SpawnState.counting;
         waveCountdown = timeBetweenWaves;
+        StartCoroutine(CountdownFrom30());
 
-        if(nextWave + 1 > waves.Length + 1)
+        if (nextWave + 1 > waves.Length + 1)
         {
             nextWave = 0;
+            Debug.Log("all waves done looping");
         }
         else
         {
             nextWave++;
             waveUpdated?.Invoke(nextWave);
-        }  
+        }
     }
 
     bool enemyisAlive()
@@ -99,16 +112,19 @@ public class WaveSpawner : MonoBehaviour
         }
         return true;
 
-}
+    }
 
     IEnumerator SpawnWave(Wave wave)
     {
+        Debug.Log("spawning Wave" + wave);
+        waveInfo.text = "Wave: " + (nextWave + 1) + " of 5";
+
         state = SpawnState.spawning;
-        for(int i = 0; i < wave.count; i++)
+        for (int i = 0; i < wave.count; i++)
         {
             SpawnEnemy(wave.enemy1);
             SpawnEnemyFly(wave.enemy2);
-            yield return new WaitForSeconds(1f/wave.rate);
+            yield return new WaitForSeconds(1f / wave.rate);
         }
 
         state = SpawnState.waiting;
@@ -116,35 +132,40 @@ public class WaveSpawner : MonoBehaviour
     }
 
     void SpawnEnemy(Transform enemy)
-    {        
+    {
+        Debug.Log("spawning enemy:" + enemy.name);
+
         if (nextWave == 0)
         {
-            Transform sp = spawnPoints[Random.Range(0, 5)];
+            Transform sp = spawnPoints[Random.Range(0, 8)];
             Instantiate(enemy, sp.position, sp.rotation);
-        }if(nextWave == 1)
+        }
+        if (nextWave == 1)
         {
-            Transform sp = spawnPoints[Random.Range(0, 5)];
+            Transform sp = spawnPoints[Random.Range(9, 16)];
             Instantiate(enemy, sp.position, sp.rotation);
         }
         if (nextWave == 2)
         {
-            Transform sp = spawnPoints[Random.Range(0, 5)];
+            Transform sp = spawnPoints[Random.Range(17, 25)];
             Instantiate(enemy, sp.position, sp.rotation);
         }
         if (nextWave == 3)
         {
-            Transform sp = spawnPoints[Random.Range(0, 5)];
+            Transform sp = spawnPoints[Random.Range(26, 34)];
             Instantiate(enemy, sp.position, sp.rotation);
         }
         if (nextWave == 4)
         {
-            Transform sp = spawnPoints[Random.Range(0, 5)];
+            Transform sp = spawnPoints[Random.Range(34, 41)];
             Instantiate(enemy, sp.position, sp.rotation);
         }
-       
+
     }
     void SpawnEnemyFly(Transform enemy)
     {
+        Debug.Log("spawning enemy:" + enemy.name);
+
         if (nextWave == 0)
         {
             Transform sp = spawnPoints[Random.Range(6, spawnPoints.Length)];
@@ -172,4 +193,23 @@ public class WaveSpawner : MonoBehaviour
         }
 
     }
+
+    IEnumerator MapcamCall()
+    {
+        Mapcam.enabled = true;
+        yield return new WaitForSeconds(5f);
+        Mapcam.enabled = false;
+    }
+
+    public IEnumerator CountdownFrom30()
+    {
+        Debug.Log("CountdownCalled");
+        for (int i = countdownTime; i > 0; i--)
+        {
+            waveInfo.text = i.ToString();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
 }
+
